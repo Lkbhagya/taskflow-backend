cd0eec18db9e0d2fc708fbc82f653eef13dfbcd9
Bhagyaifsolutions <96065582+Lkbhagya@users.noreply.github.com>
2026-06-28 09:35:44 +0530

    Ready to deploy

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const host = process.env.DB_HOST || '127.0.0.1';
-const port = process.env.DB_PORT || '3306';
+const port = Number(process.env.DB_PORT || '3306');
 const database = process.env.DB_NAME || 'taskflow';
 const username = process.env.DB_USER || 'root';
 const password = process.env.DB_PASS || '';
@@ -11,17 +11,54 @@ const sequelize = new Sequelize(database, username, password, {
   port,
   dialect: 'mysql',
   logging: false,
+
+  dialectOptions: {
+    connectTimeout: 60000,
+  },
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
-const db = { sequelize };
+const db = {
+  sequelize,
+};
 
 db.User = require('./user')(sequelize);
 db.Task = require('./task')(sequelize);
 
-db.Task.belongsTo(db.User, { foreignKey: 'assignedTo', targetKey: 'id', as: 'assignedUser', constraints: false });
-db.User.hasMany(db.Task, { foreignKey: 'assignedTo', sourceKey: 'id', as: 'assignedTasks', constraints: false });
+// Task -> Assigned User
+db.Task.belongsTo(db.User, {
+  foreignKey: 'assignedTo',
+  targetKey: 'id',
+  as: 'assignedUser',
+  constraints: false,
+});
 
-db.Task.belongsTo(db.User, { foreignKey: 'createdBy', targetKey: 'id', as: 'creator', constraints: false });
-db.User.hasMany(db.Task, { foreignKey: 'createdBy', sourceKey: 'id', as: 'createdTasks', constraints: false });
+db.User.hasMany(db.Task, {
+  foreignKey: 'assignedTo',
+  sourceKey: 'id',
+  as: 'assignedTasks',
+  constraints: false,
+});
+
+// Task -> Creator
+db.Task.belongsTo(db.User, {
+  foreignKey: 'createdBy',
+  targetKey: 'id',
+  as: 'creator',
+  constraints: false,
+});
+
+db.User.hasMany(db.Task, {
+  foreignKey: 'createdBy',
+  sourceKey: 'id',
+  as: 'createdTasks',
+  constraints: false,
+});
 
 module.exports = db;
